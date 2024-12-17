2d1ba5abd4c0134e1520483c05f4dff220e1d3cd
Emmanuel Valencia Lopera <emvalencia@unal.edu.co>
2024-12-16 19:08:06 -0500

    Prueba 7

--- zooAnimales/animal.py
+++ zooAnimales/animal.py
@@ -54,13 +54,13 @@ class Animal:
         from zooAnimales.ave import Ave
         from zooAnimales.pez import Pez
         from zooAnimales.reptil import Reptil
-        return (f"Mamiferos: {Mamifero.cantidadMamiferos()}\n",f"Aves: {Ave.cantidadAves()}\n",f"Reptiles: {Reptil.cantidadReptiles()}\n",f"Peces: {Pez.cantidadPeces()}\n",f"Anfibios: {Anfibio.cantidadAnfibios()}")
+        return f"Mamiferos: {Mamifero.cantidadMamiferos()}\nAves: {Ave.cantidadAves()}\nReptiles: {Reptil.cantidadReptiles()}\nPeces: {Pez.cantidadPeces()}\nAnfibios: {Anfibio.cantidadAnfibios()}"
 
     def toString(self):
         if self._zona is None:
-            return (f"Mi nombre es {self._nombre}, tengo una edad de {self._edad}, habito en {self._habitat} y mi genero es {self._genero}")
+            return f"Mi nombre es {self._nombre}, tengo una edad de {self._edad}, habito en {self._habitat} y mi genero es {self._genero}"
         else:
-            return (f"Mi nombre es {self._nombre}, tengo una edad de {self._edad}, habito en {self._habitat} y mi genero es {self._genero}, la zona en la que me ubico es {self._zona}, en el {self._zona.zoo}")
+            return f"Mi nombre es {self._nombre}, tengo una edad de {self._edad}, habito en {self._habitat} y mi genero es {self._genero}, la zona en la que me ubico es {self._zona}, en el {self._zona.zoo}"
 
     def movimiento(self):
         return "desplazarse"
